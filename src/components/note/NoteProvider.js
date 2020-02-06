56bcb0347ad5c81bd6fae16c4cece09d303e0b1e
philipDmartin <philipnathan788@gmail.com>
2020-02-06 14:11:46 -0600

    making a branch off master

--- src/components/note/NoteProvider.js
+++ src/components/note/NoteProvider.js
@@ -30,7 +30,7 @@ export const NoteProvider = (props) => {
     }
 
     const updateNote = note => {
-        return fetch(`http://localhost:8088/notes/${note}`, {
+        return fetch(`http://localhost:8088/notes/${note.id}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
